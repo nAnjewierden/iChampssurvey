b412b24fd98097a7cac57d2a5f62793e86b80572
Nicholas Anjewierden <nicholasanjewierden0@gmail.com>
2018-07-31 14:30:42 -0600

    Working on question editor component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      arrayOfQuestions : ['What is Your Favorite Color?!?!?!', 'another question']
+    }
+  }
+
+  captureNewQuestion(value){
+    this.setState({
+      tempQuestion : value
+    })
+  }
+
+  changeQuestion(i){
+    let newArray = this.state.arrayOfQuestions.splice(i, 1, this.state.tempQuestion)
+    console.log(newArray, this.state.arrayOfQuestions)
+    this.setState({
+      arrayOfQuestions : newArray
+    })
+  }
   render() {
+    let questionBox = this.state.arrayOfQuestions.map((ele, i) => {
+      return(
+    <div key={i}>
+    <h3>Current Question: {ele}</h3>
+      New Question : <input type='text' onChange={e => this.captureNewQuestion(e.target.value)}/>
+        <button onClick={e => this.changeQuestion(i)}>Edit</button>
+        <button>Delete</button>
+    </div>)
+    })
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+       {questionBox}
+       <button>Add Question</button>
       </div>
     );
   }
