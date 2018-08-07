0d03486edeec0944f6afdd6d8df19ae550e41940
Nicholas Anjewierden <nicholasanjewierden0@gmail.com>
2018-08-07 16:22:13 -0600

    working on editing fucntionality

--- src/Components/currentQuestionComponent.js
+++ src/Components/currentQuestionComponent.js
@@ -4,12 +4,52 @@ import axios from 'axios'
 export default class CurrentQuestionComponent extends Component{
     constructor(){
         super()
+        this.state = {
+            editable: false,
+            question: this.props.question,
+            posOrNeg: this.props.posOrNeg
+        }
+    }
+
+    handleQuestion(value){
+        this.setState({
+            question : value
+        })
+    }
+
+    handlePosOrNeg(){
+        this.setState({
+            posOrNeg : value
+        })
+    }
+
+    makeChanges(){
         
     }
+
+    makeEditable(){
+        this.setState({
+            editable : !this.state.editable
+        })
+    }
+
     render(){
-        console.log(this.props)
+        let returnable = this.state.editable ?
+        <div>
+            <input type='text' value={this.state.question} onChange={(ele) => handleQuestion(ele.target.value)}/>
+            <input type='text' value={this.state.posOrNeg} onChange={(ele) => handlePosOrNeg(ele.target.value)}/>
+            <button onClick={() => this.makeChanges()}>Make Changes</button>
+        </div>
+        :
+        <div>
+            {this.props.question} 
+            {this.props.posOrNeg ? 'positive' : 'negative'}
+            <button onClick={() => this.makeEditable()}>Edit</button>
+        </div>
+        
         return(
-            <div>{this.props.question} {this.props.posOrNeg ? 'positive' : 'negative'}</div>
+            {returnable}
         )
+        
     }
 }
