472cb47672a397202284f500e5742faae01d9e59
Nicholas Anjewierden <nicholasanjewierden0@gmail.com>
2018-11-13 16:40:14 -0700

    Chnges to outline and seed file

--- db/seedFile.sql
+++ db/seedFile.sql
@@ -1,15 +1,95 @@
-CREATE Table if not exists current_questions(
+CREATE Table if not exists survey-reponses_5(
     ID serial primary key,
-    question varchar(1000),
-    pos_or_neg boolean
+    name varchar(1000),
+    pos_or_neg boolean,
+    answer number
 );
 
-INSERT INTO current_questions (question, pos_or_neg)
-    VALUES  ('question1', true),
-            ('question2', false),
-            ('question3', true),
-            ('question4', false),
-            ('question5', true),
-            ('question6', true),
-            ('question7', false),
-            ('question8', false)
+CREATE Table if not exists current_questions_5(
+    ID serial primary key,
+    question varchar(1000),
+    pos_or_neg boolean,
+    answer number
+);
+
+INSERT INTO current_questions_5 (question, pos_or_neg, answer)
+    VALUES  ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0);
+
+CREATE Table if not exists current_questions_10(
+    ID serial primary key,
+    question varchar(1000),
+    pos_or_neg boolean,
+    answer number
+);
+
+INSERT INTO current_questions_10 (question, pos_or_neg, answer)
+    VALUES  ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0),
+            ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0);
+
+CREATE Table if not exists current_questions_15(
+    ID serial primary key,
+    question varchar(1000),
+    pos_or_neg boolean,
+    answer number
+);
+
+INSERT INTO current_questions_15 (question, pos_or_neg, answer)
+    VALUES  ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0),
+            ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0),
+            ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0);
+
+CREATE Table if not exists current_questions_20(
+    ID serial primary key,
+    question varchar(1000),
+    pos_or_neg boolean,
+    answer number
+);
+
+INSERT INTO current_questions_20 (question, pos_or_neg, answer)
+    VALUES  ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0),
+            ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0),
+            ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0),
+            ('question1', true, 0),
+            ('question2', false, 0),
+            ('question3', true, 0),
+            ('question4', false, 0),
+            ('question5', true, 0);
+            
+            
+           
