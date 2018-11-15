2e0df18af4071d220e650f03d3e3744c8e2823f2
Nicholas Anjewierden <nicholasanjewierden0@gmail.com>
2018-11-15 14:39:51 -0700

    changes to server index file for new set-up (needs to be split into controllers)

--- db/add_new_survey_reponse_10.sql
+++ db/add_new_survey_reponse_10.sql
@@ -1,3 +1,3 @@
-INSERT INTO survey_responses_10 (name, pos_or_neg, answer)
+INSERT INTO survey_responses_10 (schoolName, grade, schoolID, question, answer, pos_or_neg)
 VALUES
-    ($1, $2, $3)
+    ($1, $2, $3, $4, $5, $6)
--- server/index.js
+++ server/index.js
@@ -16,9 +16,18 @@ massive(process.env.CONNECTION_STRING).then((dbInstance) => {
     app.set('db', dbInstance)
 }).catch(err => console.log(err))
 
-app.get('/getCurrentQuestions', ((req, res) => {
+app.get('/getCurrentQuestions_5', ((req, res) => {
     const dbInstance = req.app.get('db')
-    dbInstance.get_current_questions().then(questions =>{
+    dbInstance.get_current_questions_5().then(questions =>{
+        res.status(200).send(questions)
+        console.log(questions)
+    })
+    
+}))
+
+app.get('/getCurrentQuestions_10', ((req, res) => {
+    const dbInstance = req.app.get('db')
+    dbInstance.get_current_questions_10().then(questions =>{
         res.status(200).send(questions)
         console.log(questions)
     })
@@ -33,37 +42,113 @@ app.get('/getAllQuestions', ((req, res) => {
     
 }))
 
-app.post('/addNewQuestion', (req, res) =>
-{
-    {
-        const dbInstance = req.app.get('db')
-        
-        const { question, PosOrNeg } = req.body
-        dbInstance.add_new_question([question, PosOrNeg])
-            .then(() => res.status(200).send('that workedM'))
-            .catch((err) => console.log(err))
-        }})
+app.get('/getCurrentQuestions_15', ((req, res) => {
+    const dbInstance = req.app.get('db')
+    dbInstance.get_current_questions_15().then(questions =>{
+        res.status(200).send(questions)
+        console.log(questions)
+    })
+    
+}))
 
-app.put('/updateQuestion/:id', (req, res) => {
+app.get('/getCurrentQuestions_20', ((req, res) => {
+    const dbInstance = req.app.get('db')
+    dbInstance.get_current_questions_20().then(questions =>{
+        res.status(200).send(questions)
+        console.log(questions)
+    })
+    
+}))
+
+
+
+app.put('/updateQuestion_5/:id', (req, res) => {
     {
         const dbInstance = req.app.get('db')
         
         const { question, PosOrNeg } = req.body
-        dbInstance.change_current_question([req.params.id, question, PosOrNeg])
+        dbInstance.change_current_question_5([req.params.id, question, PosOrNeg])
         .then(() => res.status(200).send('that workedM'))
         .catch((err) => console.log(err))
     }
 })
 
-app.delete('/deleteQuestion/:id', (req,res) => {
-    
-    const dbInstance = req.app.get('db')
-    
-    dbInstance.delete_current_question([req.params.id])
-    .then(() => res.status(200).send('that workedM'))
-    .catch((err) => console.log(err))
+app.put('/updateQuestion_10/:id', (req, res) => {
+    {
+        const dbInstance = req.app.get('db')
+        
+        const { question, PosOrNeg } = req.body
+        dbInstance.change_current_question_10([req.params.id, question, PosOrNeg])
+        .then(() => res.status(200).send('that workedM'))
+        .catch((err) => console.log(err))
+    }
 })
 
+app.put('/updateQuestion_15/:id', (req, res) => {
+    {
+        const dbInstance = req.app.get('db')
+        
+        const { question, PosOrNeg } = req.body
+        dbInstance.change_current_question_15([req.params.id, question, PosOrNeg])
+        .then(() => res.status(200).send('that workedM'))
+        .catch((err) => console.log(err))
+    }
+})
+
+app.put('/updateQuestion_20/:id', (req, res) => {
+    {
+        const dbInstance = req.app.get('db')
+        
+        const { question, PosOrNeg } = req.body
+        dbInstance.change_current_question_20([req.params.id, question, PosOrNeg])
+        .then(() => res.status(200).send('that workedM'))
+        .catch((err) => console.log(err))
+    }
+})
+
+app.post('/addNewSurveyResponse_5', (req, res) => {
+    {
+        const dbInstance = req.app.get('db')
+        
+        const { schoolName, grade, schoolID, question, answer, PosOrNeg } = req.body
+        dbInstance.add_new_survey_reponse_5([schoolName, grade, schoolID, question, answer, PosOrNeg])
+        .then(() => res.status(200).send('that workedM'))
+        .catch((err) => console.log(err))
+    }
+})
+
+app.post('/addNewSurveyResponse_10', (req, res) => {
+    {
+        const dbInstance = req.app.get('db')
+        
+        const { schoolName, grade, schoolID, question, answer, PosOrNeg } = req.body
+        dbInstance.add_new_survey_reponse_10([schoolName, grade, schoolID, question, answer, PosOrNeg])
+        .then(() => res.status(200).send('that workedM'))
+        .catch((err) => console.log(err))
+    }
+})
+
+app.post('/addNewSurveyResponse_15', (req, res) => {
+    {
+        const dbInstance = req.app.get('db')
+        
+        const { schoolName, grade, schoolID, question, answer, PosOrNeg } = req.body
+        dbInstance.add_new_survey_reponse_15([schoolName, grade, schoolID, question, answer, PosOrNeg])
+        .then(() => res.status(200).send('that workedM'))
+        .catch((err) => console.log(err))
+    }
+})
+
+app.post('/addNewSurveyResponse_20', (req, res) => {
+    {
+        const dbInstance = req.app.get('db')
+        
+        const { schoolName, grade, schoolID, question, answer, PosOrNeg } = req.body
+        dbInstance.add_new_survey_reponse_20([schoolName, grade, schoolID, question, answer, PosOrNeg])
+        .then(() => res.status(200).send('that workedM'))
+        .catch((err) => console.log(err))
+    }
+})
 
 
 const port = process.env.PORT
